044d601b7de0760f085c17bcea9e98d5b9795869
Michael Passetti <michaelpassetti8@gmail.com>
2020-09-13 12:05:40 +0200

    added env var to port

--- server.js
+++ server.js
@@ -28,12 +28,12 @@ app.get('/', (req, res) => {
 })
 
 app.post('/signin', (req, res) => {signin.handleSignin(req, res, db, bcrypt)});  
-app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});  // dependency injection  
+app.post('/register', (req, res) => {register.handleRegister(req, res, db, bcrypt)});    
 app.get('/profile/:id', (req, res) => {profile.handleProfileGet(req, res, db)});
 app.put('/image', (req, res) => {image.handleImagePut(req, res, db)});
 app.post('/imageurl', (req, res) => {image.handleApiCall(req, res)});
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
 	console.log('app is running');
 })
 
